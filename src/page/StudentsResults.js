3c3d5ae170578c134c296d5aebde86cc226b97c9
Merouane Mohamed Elamine <59193074+merouane2000@users.noreply.github.com>
2022-05-08 18:24:26 +0100

    Update StudentsResults.js

--- src/page/StudentsResults.js
+++ src/page/StudentsResults.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Grid, TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
@@ -45,6 +45,12 @@ const columns = [
   {
     field: "admission",
     headerName: "Admission",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "credit",
+    headerName: "Credit",
     width: 200,
     editable: true,
   },
@@ -93,6 +99,20 @@ export default function StudentsResults() {
     return coefs != 0 ? sum / coefs : 0;
   };
 
+  const calculateCredit = (subjects) => {
+    let sum = 0;
+    subjects.map((subject) => {
+      if (subject.semester === semester && subject.year === promo) {
+        let local =
+          subject.TD * subject.coefTD +
+          subject.TP * subject.coefTP +
+          subject.exam * subject.coefExam;
+        if (local > 10) sum += subject.credit;
+      }
+    });
+    return sum;
+  };
+
   const [data, setData] = useState([]);
   const [semester, setSemester] = useState("");
   const [promo, setPromo] = useState("");
@@ -112,12 +132,20 @@ export default function StudentsResults() {
   };
 
   const calculateCurrentStudentAverage = () => {
-    let clone = [...currentStudents]
-    clone = clone.map( student => {
+    let clone = [...currentStudents];
+    clone = clone.map((student) => {
       const subjects = student.subjects;
       const avg = calculateAverage(subjects);
+      const credit = calculateCredit(subjects);
+      student.credit = credit;
       student.avg = avg;
-      student.admission = avg >= 10 ? "ACCEPTED" : "REJECTED"
+      if (avg >= 10) {
+        student.admission = "ACCEPTED";
+        student.credit = 30;
+      }
+      if (avg < 10 && avg > 9.5) student.admission = "ACCEPTED / DEBT";
+      if (avg < 9.5 && avg >= 9) student.admission = "RACHA";
+      if (avg < 9) student.admission = "REJECTED";
 
       return student;
     });
@@ -125,9 +153,15 @@ export default function StudentsResults() {
   };
 
   useEffect(() => {}, [data]);
-  useEffect(() => { calculateCurrentStudentAverage() }, [currentStudents]);
-  useEffect(() => {selectCurrentStudents()}, [promo]);
-  useEffect(() => {selectCurrentStudents()}, [semester]);
+  useEffect(() => {
+    calculateCurrentStudentAverage();
+  }, [currentStudents]);
+  useEffect(() => {
+    selectCurrentStudents();
+  }, [promo]);
+  useEffect(() => {
+    selectCurrentStudents();
+  }, [semester]);
   useEffect(() => {
     axios.get("http://localhost:4000/get-student").then((res) => {
       const output = [];
@@ -140,13 +174,13 @@ export default function StudentsResults() {
 
   return (
     <div style={{ height: 600, width: "100%" }}>
-       <Grid container spacing={3} style={{padding:5}} >
-         <Grid item xs={12}>
-         <Paper className={classes.paper}>Affichage</Paper>
-         </Grid>
-         <Grid item xs={2}></Grid>
-       <Grid item xs={4}>
-       <Autocomplete
+      <Grid container spacing={3} style={{ padding: 5 }}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Affichage</Paper>
+        </Grid>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={4}>
+          <Autocomplete
             id="combo-box-demo"
             options={Promo}
             getOptionLabel={(option) => option.classYear}
@@ -167,7 +201,7 @@ export default function StudentsResults() {
         </Grid>
         <Grid item xs={2}></Grid>
         <Grid item xs={4}>
-        <Autocomplete
+          <Autocomplete
             id="combo-box-demo"
             options={Semasters}
             getOptionLabel={(option) => option.Semastername}
@@ -186,9 +220,8 @@ export default function StudentsResults() {
             )}
           />
         </Grid>
-       </Grid>
-       
-    
+      </Grid>
+
       <DataGrid
         rows={currentStudents}
         getRowId={(data) => data._id}
