d6f2bc594ea76d4d4097828406d1863587a1d9b0
Merouane Mohamed Elamine <59193074+merouane2000@users.noreply.github.com>
2022-06-22 15:32:31 +0100

    Update TeacherDashboard.js

--- src/teacher/TeacherDashboard.js
+++ src/teacher/TeacherDashboard.js
@@ -1,58 +1,42 @@
 import React, { useState, useEffect, useLocation } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TeacherInfo from "./TeacherInfo";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
+
 import axios from "axios";
-import { useNavigate  } from "react-router-dom";
-import TeacherTabel from "./TeacherTabel";
-import SideMenu from "../component/SideMenu";
-import Grid from "@material-ui/core/Grid";
+import { useNavigate } from "react-router-dom";
+import UiAppBar from "../component/UiAppBar";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: 'relative',
-  }
+    position: "relative",
+  },
 }));
 
 const TeacherDashboad = () => {
   const classes = useStyles();
-  const navigate = useNavigate ()
-  const email = sessionStorage.getItem('email')
+  const navigate = useNavigate();
+  const email = sessionStorage.getItem("email");
 
-  useEffect(()=>{
+  useEffect(() => {
     axios
-    .get(
-      "http://localhost:4000/teacher-info"
-    )
-    .then((res) => {
-      res.data.map(teacher =>{
-        if(teacher.email === email && teacher.isActive){
-          navigate('/teacher/tabel');
-        }
-
+      .get("http://localhost:4000/teacher-info")
+      .then((res) => {
+        res.data.map((teacher) => {
+          if (teacher.email === email && teacher.isActive) {
+            navigate("/teacher/tabel");
+          }
+        });
       })
-   
-    })
-    .catch((error) => {
-      console.log(error.data);
-    });
+      .catch((error) => {
+        console.log(error.data);
+      });
+  }, []);
 
-  },[])
- 
   return (
-    <div >
-       <AppBar position="absolute" color="default" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-           Teacher Dashboard 
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    < TeacherInfo />
-  
-     
+    <div>
+      <UiAppBar />
+      <TeacherInfo />
     </div>
   );
 };
